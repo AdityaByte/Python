a9876aece943595f7f81c3af9ab7d0f69ae1d95f
Aditya Pawar <149230860+AdityaByte@users.noreply.github.com>
2023-12-30 19:07:40 +0530

    Create comments.py

---
+++ comments.py
@@ -0,0 +1,17 @@
+# Comments in python
+
+print("Comments in python")
+
+# This is the single line Comments
+
+"""
+This is a multi line comment
+"""
+
+"""
+Comments can be used to explain Python code.
+
+Comments can be used to make the code more readable.
+
+Comments can be used to prevent execution when testing code.
+"""
