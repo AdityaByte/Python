ede04cedf36bc31b2e118be68d54cc16e7098893
Aditya Pawar <149230860+AdityaByte@users.noreply.github.com>
2024-04-09 23:53:24 +0530

    Update Oop.py

--- Oop.py
+++ Oop.py
@@ -6,6 +6,12 @@ class Employee:
     __id = 0
     __salary = 0
 
+    # constructor
+    def __init__(self , name , id , salary):
+        self.__name = name
+        self.__id = id
+        self.__salary = salary
+
     # self - means apne aap jo object jo create ho rha hai
     def set_name(self , name):
         self.__name = name
@@ -13,7 +19,20 @@ class Employee:
     def get_name(self):
         return self.__name
 
-obj = Employee()
-obj.set_name("aditya pawar")
-obj.get_name()
+    def set_id(self, id):
+        self.__id = id
+
+    def get_id(self):
+        return self.__id
+
+    def set_salary(self, salary):
+        self.__salary = salary
+
+    def get_salary(self):
+        return self.__salary
+
+
+obj = Employee("aditya" , 202 , 200000)
 print(obj.get_name())
+print(obj.get_id())
+print(obj.get_salary())
