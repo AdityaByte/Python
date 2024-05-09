f777625fcd1961484c4c37ffcf6a997ba8223bca
Aditya Pawar <cybergeek563@gmail.com>
2024-05-09 13:04:43 +0530

    add new file and new line in readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Python Programming language
 <br>
 Author - Aditya Pawar
+<br>
+<h1> Hello world </h1>
---
+++ constructor.py
@@ -0,0 +1,7 @@
+# constructor is a function which is called automatically when the object is created of a class like
+
+class Car:
+    def __init__(self):
+        print("this is the constructor")
+
+obj = Car() # here when we run that file it automatically calls up the __init__ function that is the constructorgir
